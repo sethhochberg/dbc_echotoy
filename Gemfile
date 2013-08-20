5af22f1af738faf4467b765abc7daf6da03e91cf
Seth Hochberg <seth@sethhochberg.com>
2013-08-20 15:19:00 -0700

    pull songs from echonest

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'multi_json'
+gem 'echonest-ruby-api'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+end
+
--- app.rb
+++ app.rb
@@ -1,8 +1,32 @@
-require 'sinatra'
+require 'echonest-ruby-api'
 
 class EchoToy < Sinatra::Base
+	configure :development do
+		require 'better_errors'
+  	use BetterErrors::Middleware
+	end
 
+	require 'multi_json'
+
+	get '/' do
+		erb :index
+	end
+
+	post '/process' do
+		song = Echonest::Song.new('3ZHSHRP1BSAO1J0E7')
+    @results = song.search(:combined => params['file'][:filename], :sort => 'artist_hotttnesss-desc', :song_type => 'studio').shuffle!
+  	
+		erb :_results, :layout => false
+	end 
 end
 
-EchoToy.run
+class Hash
+  def shuffle
+    Hash[self.to_a.sample(self.length)]
+  end
+
+  def shuffle!
+    self.replace(self.shuffle)
+  end
+end
 
